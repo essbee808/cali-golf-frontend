1db176244989f9db2e9bee56ecf09f2403a4aae3
Sheena Bonilla <sheena.myb@gmail.com>
2022-06-22 00:04:05 -0700

    Debug fetch request

--- src/course.js
+++ src/course.js
@@ -31,6 +31,4 @@ class Course {
         `
         return this.element
     }
-
-   
 }
--- src/courseService.js
+++ src/courseService.js
@@ -5,9 +5,27 @@ class CourseService{
 
     // Create course
     createCourse() {
-        debugger
-        // const course = {
-          
-        // }
+        const course = {
+            name: document.getElementById('name').value,
+            address: document.getElementById('address').value,
+            description: document.getElementById('description').value,
+            website: document.getElementById('website').value,
+            destination_id: 1
+        }
+
+        const configObj = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(course)
+        }
+
+        fetch(`${this.endpoint}/courses`, configObj)
+        .then(resp => resp.json())
+        .then(course => {
+            console.log('return', course)
+        })
+
     }
 }
